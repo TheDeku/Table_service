feb0275e98c0dd4650167c35258c4ed988cdcef8
Juan <Ju.pardob@alumnos.duoc.cl>
2020-11-04 00:08:02 -0300

    initial commit 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { GrumesModule } from './modules/gruMesa/grumes.module';
 import { Global, Module } from '@nestjs/common';
 import { Configuration } from './config/config.keys';
-import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { DatabaseModule } from './database/database.module';
-
 import { MesaModule } from './modules/mesa/mesa.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Global()
 @Module({
-  imports: [GrumesModule, ConfigModule, DatabaseModule, MesaModule],
+  imports: [GrumesModule, ConfigModule.forRoot(), DatabaseModule, MesaModule],
 })
 export class AppModule {
   static port: number | string;
